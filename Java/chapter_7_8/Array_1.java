3fadfa98f7bbfad35a98a659e4c3e4d421684eb7
xzzxwmsl <1365303748@qq.com>
2020-06-09 22:09:05 +0800

    学习了第7章一维数组

---
+++ Java/chapter_7_8/Array_1.java
@@ -0,0 +1,79 @@
+package chapter_7_8;
+
+// 数组的声明、创建、匿名数组创建，方法调用数组
+
+import sun.net.www.content.text.plain;
+
+public class Array_1 {
+    public static void main(String[] args) {
+        int[] intArray1;
+        intArray1 = new int[10];
+
+        int[] intArray2 = new int[10];
+
+        for (int i = 0; i < 5; i++) {
+            intArray1[i] = i;
+            intArray2[i] = i * i;
+        }
+        // 创建数组后默认值为0
+        for (int i = 0; i < 10; i++) {
+            intArray1[i] = i;
+            System.out.printf("intArray1[%d] : %d\tintArray2[%d] : %d\n", i, intArray1[i], i, intArray2[i]);
+        }
+
+        int[] intArray3 = { 1, 2, 3, 4, 5, 6 };
+        System.out.println(intArray3.length);
+
+        // 此处 i 是元素而非index
+        showArray(intArray3);
+
+        // 并没有把intArray2的值复制到intArray3
+        // 而是把intArray3引用设置为了intArray2的引用。 详解见NOTE.md
+
+        intArray3 = intArray2;
+        showArray(intArray3);
+
+        // arraycopy(sourceArray, srcPos, targetArray, tarPos, length);
+        System.arraycopy(intArray1, 1, intArray3, 0, 6);
+        showArray(intArray3);
+        showArray(intArray1);
+
+        // 匿名数组
+        showArray(new int[] { 1, 3, 1, 4, 5, 2, 0 });
+        // 方法返回数组
+        showArray(reserveArray(new int[] { 1, 2, 3, 4, 5 }));
+
+        // 可变长参数
+        showFunc(3,1,2,3,4,5,6);
+        showFunc(10,1,2,3,4,5,6);
+        showFunc(4, intArray3);
+        showFunc(20, intArray3);
+
+        //Arrays类
+
+
+    }
+
+    public static void showArray(int[] array) {
+        for (int i : array) {
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
+    }
+
+    public static int[] reserveArray(int[] array) {
+        int[] arrayNew = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            arrayNew[i] = array[array.length - i - 1];
+        }
+        return arrayNew;
+    }
+
+    public static void showFunc(int i, int... array) {
+        if(i>array.length) i=array.length;
+        for (int j = 0; j < i; j++) {
+            System.out.printf("%d ", array[j]);
+        }
+        System.out.println();
+    }
+}
---
+++ Java/chapter_7_8/Array_2.java
@@ -0,0 +1,54 @@
+package chapter_7_8;
+
+// Arrays 类的使用
+public class Array_2 {
+    public static void main(String[] args) {
+        int[] array1 = createRandomArray(15, 1, 100);
+        int[] array2 = createRandomArray(15, 1, 100);
+        showArray("array1", array1);
+        java.util.Arrays.sort(array1);
+        showArray("array1", array1);
+
+        showArray("array2", array2);
+        java.util.Arrays.parallelSort(array2, 1, 4);// 对array[low]~array[high-1]进行增序排序
+        showArray("array2", array2);
+
+        int[] array3 = { 9, 7, 5, 3, 1, 2, 4, 6, 8 };
+        java.util.Arrays.sort(array3);
+        System.out.printf("index of %d is %d, index of %d is %d\n", 131, java.util.Arrays.binarySearch(array3, 131), 1,
+                java.util.Arrays.binarySearch(array3, 1));// 二分查找必须为增序有序数组
+
+        int[] arrayNew1 = { 1, 2, 3, 4 };
+        int[] arrayNew2 = { 1, 2, 3, 4 };
+        int[] arrayNew3 = { 2, 1, 3, 4 };
+        System.out.println(java.util.Arrays.equals(arrayNew1, arrayNew2));
+        System.out.println(java.util.Arrays.equals(arrayNew1, arrayNew3));
+
+        // 可以使用 fill 方法填充整个数组或部分数组
+
+        java.util.Arrays.fill(array1, 1);
+        showArray("array1", array1);
+
+        java.util.Arrays.fill(array1, 1, 7, 2);
+        showArray("array1", array1);
+
+        System.out.println(java.util.Arrays.toString(array2));
+    }
+
+    public static int[] createRandomArray(int len, int low, int high) {
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++) {
+            array[i] = (int) (low + Math.random() * (high - low + 1));
+        }
+
+        return array;
+    }
+
+    public static void showArray(String str, int[] array) {
+        System.out.println(str + ":");
+        for (int i : array) {
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
+    }
+}
